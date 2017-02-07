7732e1953af21a4bf1ce5874d4ef741e9bb74f74
Jake Varley <jakevarley@gmail.com>
2017-02-07 14:17:41 -0500

    working now

--- jaccard_similarity.py
+++ jaccard_similarity.py
@@ -7,7 +7,7 @@ import os
 import shutil
 import plyfile
 
-def jaccard_similarity(mesh_filepath0, mesh_filepath1):
+def jaccard_similarity(mesh_filepath0, mesh_filepath1, grid_size):
     
     temp_mesh0_filepath = "/tmp/mesh0.ply"
     temp_mesh1_filepath = "/tmp/mesh1.ply"
@@ -22,10 +22,10 @@ def jaccard_similarity(mesh_filepath0, mesh_filepath1):
         os.remove(temp_mesh1_filepath)
 
     if os.path.exists(binvox0_filepath):
-        os.remove(temp_mesh0_filepath)
+        os.remove(binvox0_filepath)
 
     if os.path.exists(binvox1_filepath):
-        os.remove(temp_mesh1_filepath)
+        os.remove(binvox1_filepath)
     
     shutil.copyfile(mesh_filepath0, temp_mesh0_filepath)
     shutil.copyfile(mesh_filepath1, temp_mesh1_filepath)
@@ -44,7 +44,7 @@ def jaccard_similarity(mesh_filepath0, mesh_filepath1):
     #-d: specify voxel grid size (default 256, max 1024)(no max when using -e)
     #-e: exact voxelization (any voxel with part of a triangle gets set)(does not use graphics card)
     #-bb <minx> <miny> <minz> <maxx> <maxy> <maxz>: force a different input model bounding box
-    cmd_base = "binvox -e -d 40 -bb " + str(minx) + " " + str(miny) + " " + str(minz) + " " + str(maxx) + " " + str(maxy) + " " + str(maxz)
+    cmd_base = "binvox -e -d " + str(grid_size) + " -bb " + str(minx) + " " + str(miny) + " " + str(minz) + " " + str(maxx) + " " + str(maxy) + " " + str(maxz)
     
     mesh0_cmd = cmd_base + " " + temp_mesh0_filepath
     mesh1_cmd = cmd_base + " " + temp_mesh1_filepath
@@ -70,11 +70,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compute Jaccard between two meshes')
     parser.add_argument('mesh_filepaths', metavar='N', type=str, nargs='+',
                     help='meshes to compare')
+
+    parser.add_argument('--grid_size', metavar='d', type=int, default=40,
+                    help='grid size along single dimention')
+
     args = parser.parse_args()
 
-
     mesh_files = args.mesh_filepaths
+    grid_size = args.grid_size
     if len(mesh_files) != 2:
         print "wrong number of mesh files: wanted 2, got: " + str(len(mesh_files)) 
         
-    print compute_jaccard(*mesh_files)
+    print jaccard_similarity(mesh_files[0], mesh_files[1], grid_size)
