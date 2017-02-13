1d6fcf8245d7ce970a72faa00fddc512b94980f8
Jake Varley <jakevarley@gmail.com>
2017-02-13 18:46:19 -0500

    added option for exact voxelization

--- jaccard_similarity.py
+++ jaccard_similarity.py
@@ -7,7 +7,7 @@ import os
 import shutil
 import plyfile
 
-def jaccard_similarity(mesh_filepath0, mesh_filepath1, grid_size):
+def jaccard_similarity(mesh_filepath0, mesh_filepath1, grid_size=40, exact=True):
     
     temp_mesh0_filepath = "/tmp/mesh0.ply"
     temp_mesh1_filepath = "/tmp/mesh1.ply"
@@ -44,7 +44,11 @@ def jaccard_similarity(mesh_filepath0, mesh_filepath1, grid_size):
     #-d: specify voxel grid size (default 256, max 1024)(no max when using -e)
     #-e: exact voxelization (any voxel with part of a triangle gets set)(does not use graphics card)
     #-bb <minx> <miny> <minz> <maxx> <maxy> <maxz>: force a different input model bounding box
-    cmd_base = "binvox -e -d " + str(grid_size) + " -bb " + str(minx) + " " + str(miny) + " " + str(minz) + " " + str(maxx) + " " + str(maxy) + " " + str(maxz)
+    cmd_base = "binvox " 
+    if exact:
+        cmd_base += "-e "
+    
+    cmd_base += "-d " + str(grid_size) + " -bb " + str(minx) + " " + str(miny) + " " + str(minz) + " " + str(maxx) + " " + str(maxy) + " " + str(maxz)
     
     mesh0_cmd = cmd_base + " " + temp_mesh0_filepath
     mesh1_cmd = cmd_base + " " + temp_mesh1_filepath
@@ -73,12 +77,15 @@ if __name__ == "__main__":
 
     parser.add_argument('--grid_size', metavar='d', type=int, default=40,
                     help='grid size along single dimention')
+    parser.add_argument('--exact', metavar='e', type=bool, default=True,
+                    help='exact voxelization is only voxels that intersect surface')
 
     args = parser.parse_args()
 
     mesh_files = args.mesh_filepaths
     grid_size = args.grid_size
+    exact = args.exact
     if len(mesh_files) != 2:
         print "wrong number of mesh files: wanted 2, got: " + str(len(mesh_files)) 
         
-    print jaccard_similarity(mesh_files[0], mesh_files[1], grid_size)
+    print jaccard_similarity(mesh_files[0], mesh_files[1], grid_size, exact)
